d8905b61772fdaf37cc28cddd3314d116785f888
Antonbiznes <153935365+Antonbiznes@users.noreply.github.com>
2025-11-15 18:00:49 +0300

    Add files via upload

---
+++ clients.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Расписание</title>
+</head>
+<body>
+    <h3>Данная страница находится в работе! Приносим свои извинения:(</h3>
+</body>
+</html>
